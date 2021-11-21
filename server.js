4c7816ee6d69b20200848f89fc798245460cd19b
hamidlotfi92 <lotfipour.hamid@gmail.com>
2021-11-21 15:04:29 +0330

    add server to heroku

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require("express");
-
 const path = require("path");
-const port = process.env.PORT || 8080;
+const express = require("express");
 const app = express();
-
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+const publicPath = path.join(__dirname, "public");
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
 });
-app.listen(port);
+app.listen(port, () => {
+  console.log("Server is up!");
+});
